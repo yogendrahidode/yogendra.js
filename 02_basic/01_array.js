e6e8e35324339593ae2609fac1b2bf51966ea38f
yogendrahidode <yogendraghidode@gmail.com>
2026-08-17 18:58:10 +0000

    02_Array.js

---
+++ 02_basic/01_array.js
@@ -0,0 +1,42 @@
+//arrays
+
+const myArr = [0,1,2,3,4,5]
+
+const myHeros = ["ghidode", "yogendra", "deva"]
+
+const myArr2 = new Array(2,3,4,5,6,7)
+//console.log(myArr[0]);
+
+//Array methods
+
+//myArr.push(8);
+//myArr.pop();
+
+
+//myArr.unshift(9)
+//myArr.shift()
+
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(9));
+
+//const newArr = myArr.join()
+
+
+//console.log(myArr);
+//console.log(newArr);
+//console.log(typeof newArr);
+
+
+//slice  ,  splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+
---
+++ 02_basic/02_array.js
@@ -0,0 +1,14 @@
+const marvel_heros = ["thor", "ironman", "spiderman" ]
+const dc_heros = ["supermsn", "flash", "batman" ]
+
+//marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3]);
+
+
+//const allHeros = marvel_heros.concat(dc_heros)
+//console.log(allHeros);
+
+
+
