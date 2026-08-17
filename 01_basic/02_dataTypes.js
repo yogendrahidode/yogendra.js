ea399cc8c83a96bdd0a033991d0b6db411d70760
yogendrahidode <yogendraghidode@gmail.com>
2026-08-17 07:35:50 +0000

    add data types demonstration in 02_dataTypes.js

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+//"use strict";  //treat all js code as newer version
+
+//alert(3+3)  // we are using nodejs, not browser
+
+//console.log(3
+  //  +
+  //  3
+//)  //code readability should be hight
+
+let name = "Ghidode"
+let age = 22
+let isLoggedIn = true
+
+//number =>2 to power of 53
+//bigint
+//String => ""
+
+//boolean => true/false
+//null => standalone value
+//undefined =>  
+//symbol => unique
+
+
+//Object
+
+console.log(typeof "Ghidode");
+console.log(typeof undefined); //undefined
+console.log(typeof null);   //object
