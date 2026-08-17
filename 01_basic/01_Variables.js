8f04f24e4436febb9b48c736252ad2be7b559d31
yogendrahidode <yogendraghidode@gmail.com>
2026-08-17 07:10:30 +0000

    add variable declarations and initial values in 01_Variables.js

---
+++ 01_basic/01_Variables.js
@@ -0,0 +1,26 @@
+const accountId = 9098635248
+let accountEmail = "yogendraghidode@gmail.com"
+var accountPassword = "7697610630"
+accountCity = "Bhopal"
+let accountState;   //automatic undefined
+
+
+//accountId = 7697 //not change const
+
+accountEmail = "ghidode@gmail.com"
+
+accountPassword = "9098635248"
+
+accountCity = "Indore"
+
+//console.log(accountId);
+//console.log(accountEmail);
+//console.log(accountId);
+//console.log(accountCity);
+
+/*
+Not to user var
+becouse issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
