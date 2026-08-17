5d14c7a93bcc55a58c09af756ac8d4a6351bd3e9
yogendrahidode <yogendraghidode@gmail.com>
2026-08-17 08:56:34 +0000

    refactor: comment out unused console logs and operations in conversionOperation.js

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -3,8 +3,8 @@ console.log(typeof score);
 console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN
@@ -18,3 +18,30 @@ console.log(booleanIsLoggedIn);
 // 0 => false
 // "" => false
 //"Ghidode" => true
+
+
+//***********************************Operation**************************
+
+//let value = 3
+//let negValue = -value
+//console.log(negValue);
+
+//let str1 = "Ghidode"
+//let str2 = " Yogendra"
+
+//let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+
+//console.log(1 + "2");
+
+//console.log("1" + 2 + 2);
+
+//console.log(1 + 2 + "2");
+
+
+let gameCounter = 100
+gameCounter++;
+//++gameCounter;
+console.log(gameCounter)
