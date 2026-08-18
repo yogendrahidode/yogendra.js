dd5db3388d50e30e54120926cafba45711569a64
yogendrahidode <yogendraghidode@gmail.com>
2026-08-18 15:45:41 +0000

    add initial scope examples in 02_Scopes.js

---
+++ 04_basic/02_Scopes.js
@@ -0,0 +1,22 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+
+//var c = 3000    //---->globel
+let a = 5000
+
+if(true){        //--->Scops
+   let a = 10
+const b = 20
+var c = 30 
+   c = 30
+}
+
+ console.log(a);
+ //console.log(b);
+ //console.log(c);
