b89e60e055b1e28c76b5eb3c5d208250cf596629
yogendrahidode <yogendraghidode@gmail.com>
2026-08-18 16:14:43 +0000

    comment out code examples and console logs in 02_Scopes.js for clarity

--- 04_basic/02_Scopes.js
+++ 04_basic/02_Scopes.js
@@ -8,15 +8,56 @@
 
 
 //var c = 3000    //---->globel
-let a = 5000
+//let a = 5000
 
-if(true){        //--->Scops
-   let a = 10
-const b = 20
-var c = 30 
-   c = 30
-}
+//if(true){        //--->Scops
+//   let a = 10
+//const b = 20
+// var c = 30 
+//    c = 30
+// }
 
- console.log(a);
+ //console.log(a);
  //console.log(b);
  //console.log(c);
+
+
+ function one(){               //----->Nested
+    const username = "ghidode"
+
+    function two(){
+      const wedsite = "youtube"
+      console.log(username);
+    }
+    //console.log(website);
+
+    two()
+ }
+// one()
+
+
+if(true){
+    const username = "ghidode"
+
+    if(username === "ghidode"){
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+//++++++++++++++++++++++++++++++++++++  interestring  ++++++++++++++++++++++++
+
+
+console.log(addone(5));
+function addone(num){
+   return num + 1
+}
+//addone(5)
+//console.log(addTwo(5));
+const addTwo = function(num){
+    return num + 2
+}
+//addTwo(5)
