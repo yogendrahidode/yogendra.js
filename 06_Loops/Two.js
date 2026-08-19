10ef2b07c6ce2bed743baceade5b84fe9cfc39c0
yogendrahidode <yogendraghidode@gmail.com>
2026-08-19 16:54:39 +0000

    add do-while loop example in 06_Loops/Two.js

---
+++ 06_Loops/Two.js
@@ -0,0 +1,28 @@
+// while(condition){
+
+// }
+
+let index = 0;
+// while(index <=10){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;
+// }
+
+
+// let myArray = ['yogendra', 'ghidode', 'ravina', 'ghidode']
+// let arr = 0
+// while(arr<myArray.length){
+//   console.log(`value is ${myArray[arr]}`);
+//   arr = arr +1;
+// }
+
+
+// do{
+
+// }while(condition);
+
+let score = 1
+do{
+  console.log(`score  is ${score}`);
+  score++;
+}while(score <= 10);
