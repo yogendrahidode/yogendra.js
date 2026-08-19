a60729d2340e4aec79618a8d32074de726949422
yogendrahidode <yogendraghidode@gmail.com>
2026-08-19 17:56:02 +0000

    add object and array iteration examples in 06_Loops/four.js

---
+++ 06_Loops/four.js
@@ -0,0 +1,15 @@
+const myObject = {
+    js : 'javascript',
+    cpp : 'c++',
+    rd : 'ruby',
+    swift : 'swift by app'
+}
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${ myObject[key]}`);   
+}
+
+const programming =  ["js", "rb", "python", "java"]
+for (const key in programming) {
+   // console.log(programming[key]);    
+}
+
