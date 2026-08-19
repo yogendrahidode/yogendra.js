a86168055ea06b22efed6bcba3c4c84d6f93260b
yogendrahidode <yogendraghidode@gmail.com>
2026-08-19 10:32:30 +0000

    add truthy and falsy value examples in 05_flow/truthy.js

---
+++ 05_flow/truthy.js
@@ -0,0 +1,22 @@
+const userEmail = "ghidode@gmail.com"
+
+if(userEmail){
+   console.log("Got user email");   
+}
+else{
+    console.log("Dont have user email");
+}
+
+//falsy values --> false, 0, -0, BigInt, null, undefined, Nan , 0n
+
+
+//truthy value --> "0", 'false', " ", [], {}, function(){}
+
+
+// const emptyObj = {}
+// if(object.keys(emptyObj).lenght == 0){
+//   console.log("Object is empty");
+// }
+
+
+
