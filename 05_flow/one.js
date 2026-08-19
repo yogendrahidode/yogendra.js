92613c93d5d56f91e83aec12ee7e46921cdd667c
yogendrahidode <yogendraghidode@gmail.com>
2026-08-19 09:23:17 +0000

    add initial flow control examples in 05_flow/one.js

---
+++ 05_flow/one.js
@@ -0,0 +1,60 @@
+// const temperature = 41;
+
+// if (temperature < 50) {
+//     console.log("Less than 50");
+// } 
+// else
+// {
+//     console.log("Temperature greater than or equal to 50");
+// }
+
+
+
+// const score = 200
+// if(score > 100){
+//     const power = "fly"
+//     var power = "fly"
+//     console.log(`User power:${power}`);
+
+// }
+// console.log(`User power:${power}`);
+
+
+
+
+//const balance = 1000
+
+//if(balance > 500) console.log("test"),
+//console.log("test2");
+
+// if(balance < 500){
+//  console.log("Less then 500");
+// }
+// else if(balance < 750){
+// console.log("Less then 750");
+// }
+// else if(balance < 900){
+//   console.log("Less then 900");
+// }
+// else{
+//     console.log("Less then 1200");
+// }
+ 
+
+
+const userLoggedIn = true
+
+const debitcard = true
+
+const loggedInFromGoogle = false
+
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitcard){          //dono condition true
+ console.log("Allow for Shopping");
+ 
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+   console.log("User logged in");
+}
