3e960b906b5b944b080115fa1f8de37bd87ee085
yogendrahidode <yogendraghidode@gmail.com>
2026-08-19 06:17:09 +0000

    add Immediately Invoked Function Expressions (IIFE) examples in 04_iife.js

---
+++ 04_basic/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expression (iife)
+
+(function chai(){                       //named iife
+    console.log(`DB Connected`);
+    
+})();                                 //;   ->do iifi me semicolome is must
+
+(function ghidode(){
+    console.log(`Ghidode`);
+})();
+
+
+( (name) => {                                 //simple iife
+    console.log(`Yogendra ${name}`);
+})(`betul`)
