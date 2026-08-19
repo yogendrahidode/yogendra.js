303ec758689b451abe7bd3a492232873ee13b087
yogendrahidode <yogendraghidode@gmail.com>
2026-08-19 11:44:29 +0000

    add loop examples in 06_Loops/one.js

---
+++ 06_Loops/one.js
@@ -0,0 +1,33 @@
+//for
+
+// for(let index = 0; index < Array.length; i++){
+//     const element = array[index];
+// }
+
+// for(let index = 0; index < 10; index++){
+//     const element = index;
+//     console.log(element);
+    
+// }
+
+
+for(let i = 0; i < 10; i++){
+    const element = i;
+    if(element == 5){
+        //console.log("5 is the best number");
+    }
+
+    //console.log(element);
+}
+
+
+for(let i = 0; i < 10; i++){
+    //console.log(`outer loop value : ${i}`)
+    for(let j = 0; j < 10; j++){
+    //console.log(`iner loop value : ${j} and inner loop ${i}`)
+   // console.log(i + '*' + j + '=' + i*j);
+    }
+}
+
+
+//break and continue
