8eef4bcf47c76b57af70a2ec5708866be9f1f831
yogendrahidode <yogendraghidode@gmail.com>
2026-08-20 07:22:16 +0000

    add initial structure for examples in 06_Loops/six.js

---
+++ 06_Loops/six.js
@@ -0,0 +1,33 @@
+//const coding = ["js","ruby","cpp","css","html"]
+
+// const values = coding.forEach ( (item) => {
+//     //console.log(item); 
+//     return item  
+// })
+// console.log(values);
+
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) => {
+//    return num > 4
+// } )
+// console.log(newNums);
+
+
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if(num > 5){
+//        newNums.push(num)
+//     }
+
+// } )
+// console.log(newNums);
+
+
+
+
+
